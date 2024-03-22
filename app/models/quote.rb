f54c3f24a7f4e6d02c83737411c8d6c4c11040aa
Yaquelqun <sandjiv.parassou@gmail.com>
2024-03-22 16:20:29 +0100

    properly scope quotes to the company workspace

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -8,5 +8,5 @@ class Quote < ApplicationRecord
   # after_create_commit -> { broadcast_prepend_later_to "quotes", partial: "quotes/quote", locals: { quote: self}, target: "quotes" }
   # after_update_commit -> { broadcast_replace_later_to "quotes", partial: "quotes/quote", locals: { quote: self}, target: self }
   # after_destroy_commit -> { broadcast_remove_to "quotes", target: self }
-  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
+  broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
 end
